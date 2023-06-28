4ac593da7d0204c0e42bd7f4656668f0045533cb
venudinesh <venudinesh21@gmail.com>
2023-06-28 19:18:30 +0530

    nashc: Initial adaptation for Rising

--- lineage_nashc.mk
+++ lineage_nashc.mk
@@ -15,6 +15,23 @@ $(call inherit-product, device/realme/nashc/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some RisingOSS stuff.
+RISING_CHIPSET := "Mediatek Helio G95"
+RISING_MAINTAINER := Eren_Yeager
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RISING_PACKAGE_TYPE := "CORE GAPPS"
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+SUSHI_BOOTANIMATION := 1080
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := fals
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_EXCLUDES_AUDIOFX := false
+TARGET_BUILD_APERTURE_CAMERA := true
+
 PRODUCT_NAME := lineage_nashc
 PRODUCT_DEVICE := nashc
 PRODUCT_MANUFACTURER := Realme
